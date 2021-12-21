8fe15117d595d2c4f411ec2b74e34a7c2d2bb768
Ivana.Dragneva <ivanadragneva@gmail.com>
2021-12-21 19:28:24 +0200

    commit

--- project/src/components/Welcome-page.js
+++ project/src/components/Welcome-page.js
@@ -1,26 +1,34 @@
+import './Welcome-page.css';
 import React, {Component} from 'react';
-import {Button, Col, Container, Row,Form} from "react-bootstrap";
+import {Container, Navbar, Nav} from "react-bootstrap";
 
 class WelcomePage extends Component {
     render() {
         return (
             <div>
-                <Container>
-                    <Row>
-                        <Col>
-                            <h1>Welcome to the app!</h1>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Button variant={"info"}>Log in</Button>
-                        </Col>
-                        <Col>
-                            <Button variant={"info"}>Register</Button>
-                            {/*blah blah blah*/}
-                        </Col>
-                    </Row>
-                </Container>
+                <Navbar bg="light" expand="lg">
+                    <Container fluid>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav
+                                className="me-auto my-2 my-lg-0"
+                                style={{ maxHeight: '100px' }}
+                                navbarScroll
+                            >
+                                <Nav.Link>Home</Nav.Link>
+                                <Nav.Link>News</Nav.Link>
+                                <Nav.Link>About</Nav.Link>
+                                <Nav.Link>Hospitals</Nav.Link> {/*make dropdown*/}
+
+                            </Nav>
+                            <Nav className={"justify-content-end"}>
+                                <Nav.Link>Log in</Nav.Link>
+                                <Nav.Link>Register</Nav.Link>
+                            </Nav>
+
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
             </div>
         );
     }
