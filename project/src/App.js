351b7c9f8a40cd1e87979a94b16bc2bd0d319006
Ivana.Dragneva <ivanadragneva@gmail.com>
2021-12-22 18:27:17 +0200

    commit

--- project/src/App.js
+++ project/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Registration from './components/Registration'
 import Questionnaire from './containers/Questionnaire';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-    <Questionnaire/>
+    <Login/>
     </div>
   );
 }
