901796bb6cf79d0e7732f57c4d7d95ce50aacad9
IvanaDragneva <41069732+IvanaDragneva@users.noreply.github.com>
2022-01-05 20:26:55 +0200

    commit (#11)

--- project/src/App.js
+++ project/src/App.js
@@ -2,13 +2,23 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Registration from './components/Registration'
 import Questionnaire from './containers/Questionnaire';
 import Login from './components/Login';
-import Login from "./components/Login";
-import WelcomePage from "./components/Welcome-page"
+import WelcomePage from './components/Welcome-page';
+import Hospitals from './components/Hospitals';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+      <WelcomePage/>
+      <Routes>
+        <Route path='/' element={<WelcomePage/>}/>
+        <Route path='/Hospitals' element={<Hospitals/>}/>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/Registration' element={<Registration/>}/>
+      </Routes>
+      </Router>
     <Login/>
     <Registration/>
     </div>
--- project/src/components/Welcome-page.js
+++ project/src/components/Welcome-page.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import {Container, Navbar, Nav} from "react-bootstrap";
 import Login from './Login';
 import Registration from './Registration';
+import { Link } from "react-router-dom"
 
 class WelcomePage extends Component {
     render() {
@@ -17,9 +18,23 @@ class WelcomePage extends Component {
                                 style={{ maxHeight: '100px' }}
                                 navbarScroll
                             >
-                                <Nav.Link>Home</Nav.Link>
+                                <Nav.Link>
+                                    <Link to="WelcomePage">Home</Link>
+                                </Nav.Link>
                                 <Nav.Link>News</Nav.Link>
                                 <Nav.Link>About</Nav.Link>
+                                <Nav.Link>
+                                    <Link to="Hospitals">Hospitals</Link>
+                                </Nav.Link>
+
+                            </Nav>
+                            <Nav className={"justify-content-end"}>
+                                <Nav.Link>
+                                    <Link to="Login">Log in</Link>
+                                </Nav.Link>
+                                <Nav.Link>
+                                    <Link to="Registration">Register</Link>
+                                </Nav.Link>
                                 <Nav.Link>Hospitals</Nav.Link>
                                 <Nav.Link>Hospitals</Nav.Link>
                             </Nav>
@@ -27,7 +42,6 @@ class WelcomePage extends Component {
                                 <Nav.Link href="/login">Log in</Nav.Link>
                                 <Nav.Link href="/registration">Register</Nav.Link>
                             </Nav>
-
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
